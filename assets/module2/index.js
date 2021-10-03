cd7993fa36fba08b44795d6c208fa408631e3dee
julietvt <freshitpt@gmail.com>
2021-10-03 12:12:25 +0300

    export many objects as one

--- assets/module2/index.js
+++ assets/module2/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import { sum, sub, mult, div } from '../module1/index.js';
+import * as calc from '../module1/index.js';
 
-console.log(sum(2, 5));
-console.log(sub(2, 5));
-console.log(mult(2, 5));
-console.log(div(2, 5));
+console.log(calc.sum(2, 5));
+console.log(calc.sub(2, 5));
+console.log(calc.mult(2, 5));
+console.log(calc.div(2, 5));
