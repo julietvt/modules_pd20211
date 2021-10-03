c0b30b70a3a03b0e52459e40d148abc63eec62d9
julietvt <freshitpt@gmail.com>
2021-10-03 12:29:42 +0300

    export with alias (as)

--- assets/module2/index.js
+++ assets/module2/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-import greetingMsg, * as calc from '../module1/index.js';
+import {
+  sub as subWithTwoValue,
+  sum as sumWithTwoValue,
+} from '../module1/index.js';
 
-console.log(greetingMsg);
-console.log(calc.sum(2, 5));
-console.log(calc.sub(2, 5));
-console.log(calc.mult(2, 5));
-console.log(calc.div(2, 5));
-console.log('------');
-console.log(this);
+console.log(sumWithTwoValue('2', '5'));
+console.log(subWithTwoValue(2, 5));
