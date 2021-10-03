8140f2c3240f087e9a129c7dc535ae9080bfa85b
julietvt <freshitpt@gmail.com>
2021-10-03 12:09:32 +0300

    export many objects

--- assets/module1/index.js
+++ assets/module1/index.js
@@ -3,3 +3,15 @@
 export function sum(value1, value2) {
   return value1 + value2;
 }
+
+export function sub(value1, value2) {
+  return value1 - value2;
+}
+
+export function mult(value1, value2) {
+  return value1 * value2;
+}
+
+export function div(value1, value2) {
+  return value1 / value2;
+}
--- assets/module2/index.js
+++ assets/module2/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
-import { sum } from '../module1/index.js';
+import { sum, sub, mult, div } from '../module1/index.js';
 
 console.log(sum(2, 5));
+console.log(sub(2, 5));
+console.log(mult(2, 5));
+console.log(div(2, 5));
